a1862ebfc3f58f2de57b325585a42046bf3264dd
Evan Pascual <elp9@njit.edu>
2025-02-24 23:47:21 -0500

    finished login/register/validate

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,41 +4,45 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function doLogin($uname, $passwd) {
+function doLogin($uname, $passwd, $sesStart) {
     $mysqli = require __DIR__ . "/database.php";
-    //if (!$mysqli || !($mysqli instanceof mysqli)) {
-    //    return array("returnCode" => '0', 'message' => "Database connection failed");
-    // }
-
-    $sql = "SELECT password FROM user_login WHERE username = ?";
+    
+    $sql = "SELECT user_id, password FROM user_login WHERE username = ?";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("s", $uname);
     $stmt->execute();
     $result = $stmt->get_result();
-
+    
     if ($user = $result->fetch_assoc()) {
-	if ($passwd == $user["password"]) {
-		return array("returnCode" => '1', 'message' => "Login Successful");
+	    $userID = $user['user_id'];
+	    $exp_date = $sesStart + 3600;
+
+        if (password_verify($passwd, $user["password"])) {
+            $insertSql = "INSERT INTO sessions (user_id, session_start, session_expires) VALUES (?, ?, ?)";
+            $insertStmt = $mysqli->prepare($insertSql);
+            $insertStmt->bind_param("iii", $userID, $sesStart, $exp_date);
+            $insertStmt->execute();
+            return array("returnCode" => '1', 'message' => "Login Successful");
         } else {
-		return array("returnCode" => '0', 'message' => "Wrong password");
+            return array("returnCode" => '0', 'message' => "Invalid input");
         }
     } else {
-	    return array("returnCode" => '0', 'message' => "User not found");
+        return array("returnCode" => '0', 'message' => "Invalid username");
     }
 }
 
-function doRegister($fname, $lname, $email, $passwd, $uname)
+function doRegister($fname, $lname, $email, $uname, $passwd)
 {
    $passhash = password_hash($passwd, PASSWORD_DEFAULT);	
    $mysqli = require __DIR__ . "/database.php";
-   $sql = "INSERT INTO user_login (f_name, l_name, email, password, created_at, username)
-	   VALUES (?, ?, ?, ?, CURDATE(), ?)";
+   $sql = "INSERT INTO user_login (f_name, l_name, email, username, password, created_at)
+	   VALUES (?, ?, ?, ?, ?, ?)";
    $stmt = $mysqli->stmt_init();
    if (!$stmt->prepare($sql)) {
       return array("returnCode" => "0", "message" => 'statement prepare error');
    }
-
-   $stmt->bind_param("sssss", $fname, $lname, $email, $uname, $passwd);
+   $d = time();
+   $stmt->bind_param("sssssi", $fname, $lname, $email, $uname, $passhash, $d);
    if ($stmt->execute()) {
       return array ("returnCode" => "1", "message" => 'success');
    } else {
@@ -51,20 +55,20 @@ function doRegister($fname, $lname, $email, $passwd, $uname)
 
 }
 
-function doValidate(/*$sessionID, $userID,*/ $sessionData, $sesStart)
+function doValidate(/*$userID,*/ $sessionData, $sesStart)
 {
 
 $mysqli = require __DIR__ . "/database.php";
-$sql = "INSERT INTO sessions (user_id, session_data, session_start)
-	VALUES (3, ?, ?)";
+$sql = "INSERT INTO sessions (session_data, session_start)
+	VALUES (?, ?)";
 
 $stmt = $mysqli->stmt_init();
    if (!$stmt->prepare($sql)) {
       return array("returnCode" => "0", "message" => 'statement prepare error');
    }
-$stmt->bind_param("si", /*$sessionID, $userID,*/ $sessionData, $sesStart);
+$stmt->bind_param("si",/* $userID*/$sessionData, $sesStart);
 if ($stmt->execute()) {
-      return array ("returnCode" => "2", "message" => 'success');
+      return array ("returnCode" => "1", "message" => 'success');
    } else {
        return array("returnCode" => "0", "message" => 'statement execution failed');
    }
@@ -82,9 +86,9 @@ function requestProcessor($request)
   switch ($request['type'])
   {
     case "login":
-      return doLogin($request['username'],$request['password']);
+      return doLogin($request['username'],$request['password'], $request['session']);
     case "validate_session":
-	    return doValidate(/*$request['session_id'], $request['user_id'],*/ $request['session_data'], $request['session_start'] /*$request['session_expires']*/);
+	    return doValidate($request['session_data'], $request['session_start']);
     case "register":
       return doRegister($request['f_name'], $request['l_name'], $request['email'], 
 	  $request['username'], $request['password']);
