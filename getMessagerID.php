7966b775e961698fd9f486d018e34b7df1c293e8
Evan Pascual <elp9@njit.edu>
2025-04-02 16:48:44 -0400

    fixed violation

--- getMessagerID.php
+++ getMessagerID.php
@@ -3,13 +3,17 @@
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
 require_once "vendor/autoload.php";
+use Dotenv\Dotenv;
 
 use Twilio\Rest\Client;
 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-$sid = 'ACc503a6379dfe2f243d7964aae894a770';
-$token = '20ff77056c773ad0772f065772552fb1';
+$sid = $_ENV('TWILIO_SID');
+$token = $_ENV('TWILIO_TOKEN');
 $twilio = new Client($sid, $token);
 
 $service = $twilio->messaging->v1->services->create(
